c19f421fe3e2934a84e39815bd779dcf3c958748
tintin <906183742@qq.com>
2020-05-24 12:56:27 +0800

    博客comment层级展示优化

--- src/main/java/com/tintin/blog/dao/CommentRepository.java
+++ src/main/java/com/tintin/blog/dao/CommentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment,Long>{
+public interface CommentRepository extends JpaRepository<Comment,Long> {
 
-    List<Comment> findByBlogId(Long blogId, Sort sort);
+    List<Comment> findByBlogIdAndParentCommentNull(Long blogId, Sort sort);
 }
--- src/main/java/com/tintin/blog/service/CommentServiceImpl.java
+++ src/main/java/com/tintin/blog/service/CommentServiceImpl.java
@@ -2,10 +2,12 @@ package com.tintin.blog.service;
 
 import com.tintin.blog.dao.CommentRepository;
 import com.tintin.blog.po.Comment;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -18,7 +20,8 @@ public class CommentServiceImpl implements CommentService{
     @Override
     public List<Comment> listCommentByBlogId(Long blogId) {
         Sort sort = Sort.by(Sort.Direction.DESC,"createTime");
-        return commentRepository.findByBlogId(blogId,sort);
+        List<Comment> comments = commentRepository.findByBlogIdAndParentCommentNull(blogId,sort);
+        return eachComment(comments);
     }
 
     @Override
@@ -33,5 +36,58 @@ public class CommentServiceImpl implements CommentService{
         }
         comment.setCreateTime(new Date());
         return commentRepository.save(comment);
+    }
+
+    /**
+     * 拷贝一分所有的顶级comment，并且执行组合对应子comment
+     * @param comments
+     * @return
+     */
+    private List<Comment> eachComment(List<Comment> comments){
+        List<Comment> commentsView = new ArrayList<>();
+        for (Comment comment:comments){
+            Comment c = new Comment();
+            BeanUtils.copyProperties(comment,c);
+            commentsView.add(c);
+        }
+        combineChildren(commentsView);
+        return commentsView;
+    }
+
+    /**
+     * 将顶级comment的子comment组合
+     * @param comments
+     */
+    private void combineChildren(List<Comment> comments){
+        for (Comment comment:comments){
+            List<Comment> replys1 = comment.getReplyComments();
+            for (Comment reply1:replys1){
+                // 设置对应的父级comment
+                reply1.setParentComment(comment);
+                recursively(reply1);
+            }
+            comment.setReplyComments(tempReplys);
+            tempReplys = new ArrayList<>();
+        }
+    }
+
+    private List<Comment> tempReplys = new ArrayList<>();
+
+    /**
+     * 递归子comment
+     * @param comment
+     */
+    private void recursively(Comment comment){
+        tempReplys.add(comment);
+        if(comment.getReplyComments().size()>0){
+            List<Comment> replys = comment.getReplyComments();
+            for(Comment reply:replys){
+                reply.setParentComment(comment);
+                tempReplys.add(reply);
+                if(reply.getReplyComments().size()>0){
+                    recursively(reply);
+                }
+            }
+        }
     }
 }
