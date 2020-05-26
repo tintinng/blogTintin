807a97bd347fe9fe22286e50d8cce4ec0c13ba7f
tintin <906183742@qq.com>
2020-05-26 22:44:07 +0800

    归档

--- src/main/java/com/tintin/blog/dao/BlogRepository.java
+++ src/main/java/com/tintin/blog/dao/BlogRepository.java
@@ -22,4 +22,10 @@ public interface BlogRepository extends JpaRepository<Blog,Long>, JpaSpecificati
     @Modifying
     @Query("update Blog b set b.views = b.views+1 where b.id = ?1")
     int updateViews(Long id);
+
+    @Query("select function('date_format',b.updateTime,'%Y') as year from Blog b group by year order by function('date_format',b.updateTime,'%Y') desc ")
+    List<String> findGroupYear();
+
+    @Query("select b from Blog b where function('date_format',b.updateTime,'%Y') = ?1")
+    List<Blog> findBlogByYear(String year);
 }
--- src/main/java/com/tintin/blog/service/BlogServiceImpl.java
+++ src/main/java/com/tintin/blog/service/BlogServiceImpl.java
@@ -18,9 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.criteria.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class BlogServiceImpl implements BlogService{
@@ -112,6 +110,22 @@ public class BlogServiceImpl implements BlogService{
         Sort sort = Sort.by(Sort.Order.desc("updateTime"));
         Pageable pageable = PageRequest.of(0, size, sort);
         return blogRepository.findTop(pageable);
+    }
+
+    @Override
+    public Map<String, List<Blog>> archiveBlog() {
+        List<String> years = blogRepository.findGroupYear();
+        Map<String,List<Blog>> map = new HashMap<>();
+        for (String year:years) {
+            map.put(year,blogRepository.findBlogByYear(year));
+        }
+
+        return map;
+    }
+
+    @Override
+    public Long countBlog() {
+        return blogRepository.count();
     }
 
     @Transactional
